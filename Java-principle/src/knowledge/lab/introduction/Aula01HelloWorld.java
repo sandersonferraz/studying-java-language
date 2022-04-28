82dad383559f63c3e5a2ea4121ee29eb7d5532dd
Sanderson Ferraz <sandersonferraz@gmail.com>
2022-04-28 00:07:58 -0300

    Hello worls and primitive types af the variables.

---
+++ Java-principle/src/knowledge/lab/introduction/Aula01HelloWorld.java
@@ -0,0 +1,18 @@
+package knowledge.lab.introduction;
+
+public class Aula01HelloWorld {
+    /**
+     *
+     * <b> Comentarios devem ser evitados.</b>
+     * @param args
+     */
+    public  static void main (String[] args){
+        // Comentario de uma linha so.
+        /*
+            Comentario de multiplas linhas.
+            Linha 2
+            Linha 3
+         */
+        System.out.println("Hello World.");
+    }
+}
---
+++ Java-principle/src/knowledge/lab/introduction/Aula02PrimitiveTypes.java
@@ -0,0 +1,11 @@
+package knowledge.lab.introduction;
+
+public class Aula02PrimitiveTypes {
+
+    public static void main(String[] args) {
+        // int, double, float, char, byte, short, long, boolean
+        int age = 10;
+
+        System.out.println("You age is: " + age + "years");
+    }
+}
